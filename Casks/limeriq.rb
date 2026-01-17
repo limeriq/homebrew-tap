ee4b392078ca3aaed4cd0adbd82f91f50b24aa74
Darren Apfel <darren@theapfels.org>
2026-01-16 22:21:17 -0800

    cask: limeriq 0.1.1-beta.20260117

--- Casks/limeriq.rb
+++ Casks/limeriq.rb
@@ -1,6 +1,6 @@
 cask "limeriq" do
-  version "0.1.1-beta.20260116.1"
-  sha256 "6744469f7715ffa5b1a1921506f8b8dce4ba61b3a98d6f99cd028a0262a76a19"
+  version "0.1.1-beta.20260117"
+  sha256 "0156e7e1c52c9dfd1a481214068c0de4a9b711b60b0c27a46ee121d3df7bffa5"
 
   url "https://github.com/limeriq/limeriq/releases/download/v#{version}/limeriq-suite-beta.pkg"
   name "limerIQ"
